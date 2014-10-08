841238c6c36c39560bd1b3f6b2615d302153be3d
soulgain <falcon.cjj@gmail.com>
2014-10-08 11:45:05 +0800

    enhance demo.

--- demo.py
+++ demo.py
@@ -7,13 +7,28 @@ from flask import request
 import DBModel
 import json
 import mongoengine
+import plistlib
+
 
 app = Flask(__name__)
 mongoengine.connect(db='train')
+stations = plistlib.readPlist('StationList.plist')['stations']
+stationCache = {}
+
+for _,station in enumerate(stations):
+	stationCode = station['code']
+	stationCache[stationCode] = station
+
+
+def findStationByCode(stationCode):
+	global stationCache
+	return stationCache[stationCode]
 
 
 @app.route('/connection')
 def connect():
+	global stations
+
 	fromStationCode = request.args.get('from', '')
 	toStationCode = request.args.get('to', '')
 	connection_set = DBModel.TrainConnectionRecord.objects(__raw__={'fromStationCode':fromStationCode, 'toStationCode':toStationCode})
@@ -23,7 +38,13 @@ def connect():
 	else:
 		connectionRecord = connection_set.first()
 		paths = connectionRecord.paths
-		return json.dumps({'paths':paths})
+
+		for _, path in enumerate(paths):
+			for index, stationCode in enumerate(path):
+				station = findStationByCode(stationCode)
+				path[index] = station['name']
+
+		return json.dumps({'paths':paths}, ensure_ascii=False)
 
 
 if __name__ == '__main__':
