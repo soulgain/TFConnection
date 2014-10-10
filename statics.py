42095645b0799919a70343c5d3ae8e93daa05f8d
soulgain <falcon.cjj@gmail.com>
2014-10-10 13:37:52 +0800

    add other statics.

--- statics.py
+++ statics.py
@@ -3,14 +3,18 @@
 
 
 from DBModel import TrainConnectionRecord
+from DBModel import TrainRecord
 import cPickle as pickle
 import mongoengine
 import time
 import os
 import sys
+import plistlib
+import json
 
 
 mongoengine.connect(host='192.168.2.1', db='train')
+stations = plistlib.readPlist('StationList.plist')['stations']
 
 
 def statics_path_count():
@@ -44,6 +48,40 @@ def show_statics_path_count():
 	print result
 
 
+def statics_station_weight():
+	result = {}
+
+	for station in stations[:]:
+		trainSet = TrainRecord.objects(fromStationCode=station['code'])
+
+		tmp = {}
+		for train in trainSet:
+			if tmp.has_key(train.trainno):
+				pass
+			else:
+				tmp[train.trainno] = 1
+
+		result[station['name'].encode('utf-8')] = len(tmp.keys())
+
+	with open('result', 'w') as file:
+		json.dump(result, fp=file, ensure_ascii=False)
+
+
+def show_statics_station_weight():
+	result = None
+	with open('result', 'r') as file:
+		result = json.load(file)
+
+	content = []
+	for key in result:
+		content.append(key+' '+str(result[key]))
+
+	with open('tmp', 'w') as file:
+		file.write('\n'.join(content).encode('utf-8'))
+
+
 if __name__ == '__main__':
-	statics_path_count()
-	show_statics_path_count()
+	# statics_path_count()
+	# show_statics_path_count()
+	# statics_station_weight()
+	show_statics_station_weight()
