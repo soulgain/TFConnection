3b3cc77a425e48d9dfb5feb382bb7bb78d1ea4a5
soulgain <falcon.cjj@gmail.com>
2014-10-16 11:04:04 +0800

    reformat, fix warnings.

--- statics.py
+++ statics.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 
 
 from DBModel import TrainConnectionRecord
 from DBModel import TrainRecord
 import cPickle as pickle
 import mongoengine
-import time
-import os
-import sys
 import plistlib
 import json
 from config import config
@@ -19,70 +16,69 @@ stations = plistlib.readPlist('StationList.plist')['stations']
 
 
 def statics_path_count():
-	result = {}
-	records = TrainConnectionRecord.objects
-	total = TrainConnectionRecord.objects.count()
+    result = {}
+    records = TrainConnectionRecord.objects
+    total = TrainConnectionRecord.objects.count()
 
-	print '\rstart...',
+    print '\rstart...',
 
-	current = 0
-	for index, connection in enumerate(records):
-		if current != str(int(float(index)/total*100)):
-			current = str(int(float(index)/total*100))
-			print(current)
-			
-		path_count = str(len(connection.paths))
+    current = 0
+    for index, connection in enumerate(records):
+        if current != str(int(float(index) / total * 100)):
+            current = str(int(float(index) / total * 100))
+            print(current)
 
-		if path_count in result:
-			result[path_count] = result[path_count]+1
-		else:
-			result[path_count] = 1
+        path_count = str(len(connection.paths))
 
-	with open('result', 'w') as file:
-		pickle.dump(result, file)
+        if path_count in result:
+            result[path_count] = result[path_count] + 1
+        else:
+            result[path_count] = 1
+
+    with open('result', 'w') as fp:
+        pickle.dump(result, fp)
 
 
 def show_statics_path_count():
-	with open('result', 'r') as file:
-		result = pickle.load(file)
+    with open('result', 'r') as fp:
+        result = pickle.load(fp)
 
-	print result
+    print result
 
 
 def statics_station_weight():
-	result = {}
+    result = {}
 
-	for station in stations[:]:
-		trainSet = TrainRecord.objects(fromStationCode=station['code'])
+    for station in stations[:]:
+        trainSet = TrainRecord.objects(fromStationCode=station['code'])
 
-		tmp = {}
-		for train in trainSet:
-			if tmp.has_key(train.trainno):
-				pass
-			else:
-				tmp[train.trainno] = 1
+        tmp = {}
+        for train in trainSet:
+            if train.trainno in tmp:
+                pass
+            else:
+                tmp[train.trainno] = 1
 
-		result[station['name'].encode('utf-8')] = len(tmp.keys())
+        result[station['name'].encode('utf-8')] = len(tmp.keys())
 
-	with open('result', 'w') as file:
-		json.dump(result, fp=file, ensure_ascii=False)
+    with open('result', 'w') as fp:
+        json.dump(result, fp=fp, ensure_ascii=False)
 
 
 def show_statics_station_weight():
-	result = None
-	with open('result', 'r') as file:
-		result = json.load(file)
+    with open('result', 'r') as fp:
+        result = json.load(fp)
 
-	content = []
-	for key in result:
-		content.append(key+' '+str(result[key]))
+    content = []
+    for key in result:
+        content.append(key + ' ' + str(result[key]))
 
-	with open('tmp', 'w') as file:
-		file.write('\n'.join(content).encode('utf-8'))
+    with open('tmp', 'w') as fp:
+        fp.write('\n'.join(content).encode('utf-8'))
 
 
 if __name__ == '__main__':
-	# statics_path_count()
-	# show_statics_path_count()
-	# statics_station_weight()
-	show_statics_station_weight()
+    # statics_path_count()
+    # show_statics_path_count()
+    # statics_station_weight()
+    show_statics_station_weight()
